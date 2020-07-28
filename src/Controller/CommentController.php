6db745df5a465a94eafda6226a9467527e70d75c
dseagren <drseagren@gmail.com>
2020-07-28 12:14:18 -0500

    pre webpack

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,16 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommentController extends AbstractController
 {
     /**
-     * @Route("/comments/{id}/vote/{direction}")
+     * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
      */
-    public function commentVote($id, $direction)
+    public function commentVote($id, $direction, LoggerInterface $logger)
     {
         //todo use id to query database
 
         if($direction === 'up'){
+            $logger->info('Voting Up');
             $currentVoteCount = rand(7,100);
         }else{
             $currentVoteCount = rand(0,5);
+            $logger->info('Voting Down');
         }
 
         return $this->json(['votes' => $currentVoteCount]);
